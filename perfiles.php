0cb26c8c91ff072686c7994fc8ac7fb14b0fae06
servipcompu <126924544+servipcompu@users.noreply.github.com>
2023-12-05 09:49:30 -0500

    Create perfiles.php

---
+++ perfiles.php
@@ -0,0 +1,59 @@
+<?php namespace Centinela\Models;
+
+use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Behavior\Timestampable;
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Uniqueness;
+use Phalcon\Validation\Validator\PresenceOf;
+
+class Perfiles extends Model
+{
+    public $id;
+    public $mtime;
+    public $ctime;
+    public $nombre;
+    public $caption;
+    public $permanente;
+    public $activo;
+
+    public function initialize()
+    {
+        $this->hasMany('id',__NAMESPACE__.'\Usuarios','perfilId',[
+            'alias'     =>'usuarios',
+            'foreignKey'=>[
+                'message'=>'No puede ser borrado porque está siendo usado en Usuarios',
+            ],
+        ]);
+        $this->hasMany('id',__NAMESPACE__.'\PrivilegiosAcciones','perfilId',[
+            'alias'     =>'privilegiosAcciones',
+            'foreignKey'=>[
+                'message'=>'No puede ser borrado porque está siendo usado en '.
+                    'PrivilegiosAcciones',
+            ],
+        ]);
+        $this->addBehavior(new Timestampable([
+            'beforeCreate'=>[
+                'field'=>'ctime',
+                'format'=>'Y-m-d H:i:s',
+            ],
+            'beforeUpdate'=>[
+                'field'=>'mtime',
+                'format'=>'Y-m-d H:i:s',
+            ],
+        ]));
+    }
+
+    public function validation()
+    {
+        $validator = new Validation();
+        // Valida que los emails sean unicos por usuario
+        $validator->add('nombre',new Uniqueness([
+            'message' => 'El nombre del perfil ya fue registrado',
+        ]));
+        // Valida que los nombres no esten vacios despues de aplicar el trim
+        $validator->add('caption',new PresenceOf([
+            'message' => 'El caption no puede estar vacío',
+        ]));
+        return $this->validate($validator);
+    }
+}
